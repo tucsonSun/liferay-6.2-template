48b9253d32e3eba1f194f935ee3972de6e48718f
E Rendon <x_ernie_x@hotmail.com>
2014-10-12 19:10:56 -0700

    css continued development

--- liferay-plugins-sdk-6.2/themes/shock-baww-theme/docroot/_diffs/js/custom.js
+++ liferay-plugins-sdk-6.2/themes/shock-baww-theme/docroot/_diffs/js/custom.js
@@ -7,36 +7,30 @@ jQuery(document).ready(function() {
 		window.location = '/web/guest/home?p_p_id=3&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_3_struts_action=%2Fsearch%2Fsearch&_3_redirect=%2Fweb%2Fguest%2F&_3_keywords='+$(this).children('input').val()+'&_3_groupId=0&x=15&y=15';
 	});
 
-	$('.sign-in a').text("My Account");
+	$('.sign-in a').text("Admin Home");
 
 	$('.result').appendTo('.search-layout');
-
-	if ($('#cpmVersion:contains("CP-PROD")').length > 0) {
-	    $("#cpmVersion").hide();
-	}
-
-	
 
 
 	/**
 	 * Method used to disable breadcrumb top parent link for the bread crumbs
 	 * Method executes when HTML-Document is loaded and DOM is ready
 	 * 1) Find the breadcrumb top parent element
-	 * 2) If topParent exists and not equal to 'My Account' disable href
+	 * 2) If topParent exists and not equal to 'Admin Home' disable href
 	 */
 
 	//Find the breadcrumb top parent element
-	//If it exists and not equal to 'My Account' disable
+	//If it exists and not equal to 'Admin Home' disable
 	var liElement = $( "ul.breadcrumb li.first" );
 	//If exists then disable link
 	if (liElement.length) {
 		var aElement=liElement.children(":first");
 		var value = $(aElement).text();
-		if(value !="My Account") {
+		if(value !="Admin Home") {
 			//disable link
 			$(aElement).attr('href','javascript:void(0)');
 			//disable css style on link
-			$(aElement).attr('style','cursor:default; color: #00325b;');
+			$(aElement).attr('style','cursor:default; color: $yellow;');
 		}
 	}
 
