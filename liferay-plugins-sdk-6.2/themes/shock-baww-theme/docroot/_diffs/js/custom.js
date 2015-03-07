39f2f2f699b31260f0b887b38280defa5f9e34f0
tucsonSun <x_ernie_x@hotmail.com>
2015-03-06 22:00:14 -0700

    responsive changes

--- liferay-plugins-sdk-6.2/themes/shock-baww-theme/docroot/_diffs/js/custom.js
+++ liferay-plugins-sdk-6.2/themes/shock-baww-theme/docroot/_diffs/js/custom.js
@@ -16,6 +16,16 @@ jQuery(document).ready(function() {
 		var searchValue = $('#search-control-input').val();
 		window.location = '/web/guest/welcome?p_p_id=3&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_3_struts_action=%2Fsearch%2Fsearch&_3_redirect=%2Fweb%2Fguest%2F&_3_keywords='+searchValue+'&_3_groupId=0&x=15&y=15';
 	});
+	
+	/** 
+	 * Method modifies Liferay search field form submit.
+	 * -intercept the form submit and redirect search results 
+	**/
+	$(document).on('submit', '#mobile-search-controls form', function(event) {
+		event.preventDefault();
+		var searchValue = $('#search-control-input').val();
+		window.location = '/web/guest/welcome?p_p_id=3&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_3_struts_action=%2Fsearch%2Fsearch&_3_redirect=%2Fweb%2Fguest%2F&_3_keywords='+searchValue+'&_3_groupId=0&x=15&y=15';
+	});
 
 	/**
 	 *  Method for mobile responsive when the #nav-pop button is clicked 
