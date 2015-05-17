f9e44124520a5575996c802ea19a3431015304a3
tucsonSun <x_ernie_x@hotmail.com>
2015-05-17 10:23:07 -0700

    added example shared framework

---
+++ liferay-plugins-sdk-6.2/shared/EGR-Shared-Framework-Example/src/com/egr/utility/IExample.java
@@ -0,0 +1,29 @@
+/*
+ * File Name: IExample.java
+ * 
+ * Created by: Ernesto Rendon on May 17, 2015 7:43:26 AM.
+ * 
+ * Copyright (c) 2015 EGR Software Inc. 3019 E. Cortez, Arizona, 85028, U.S.A.
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of EGR Software
+ * Inc. You shall not disclose such confidential information and shall use it
+ * only in accordance with the terms of the license agreement you entered into
+ * with EGR Software Inc.
+ */
+package com.egr.utility;
+
+/**
+ * 
+ * A instance of class type IExample is used to ...
+ * 
+ * @author Ernesto Rendon
+ * @version 1.0
+ */
+public interface IExample {
+
+
+	
+	public static final String A_TEST_VARIABLE="blahh";	
+	
+}
