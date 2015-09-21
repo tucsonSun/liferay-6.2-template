5c46588f05ee80b90b3de89737b22ad311f877fb
tucsonSun <x_ernie_x@hotmail.com>
2015-09-20 22:02:05 -0700

    static change to perms classes

---
+++ liferay-plugins-sdk-6.2/portlets/rest-portlet/docroot/WEB-INF/src/com/egr/rest/commands/perms/IPermission.java
@@ -0,0 +1,60 @@
+/*
+ * File Name: IPermission.java 
+ *
+ * Created by: Ernesto Rendon on Sep 20, 2015 9:59:42 PM.
+ *
+ * Copyright (c) 2015 EGR Software Inc.
+ * 3019 E. Cortez St. Phoenix, Arizona, 85028, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * EGR Software Inc. You shall not disclose such confidential
+ * information and shall use it only in accordance with the terms of
+ * the license agreement you entered into with EGR Software Inc..
+ */
+package com.egr.rest.commands.perms;
+
+/**
+ * A instance of class type IPermission is used to ...
+ * 
+ * @author Ernesto Rendon
+ * @version 1.0
+ */
+public interface IPermission {
+
+	public static final String EMPLOYEE_TYPE = "employee";
+	public static final String CUSTOMER_TYPE = "customer";
+	public static final String BROKER_TYPE = "broker";
+	public static final String UNKNOWN_USER_TYPE = "UNKNOWN";
+	
+	public static final String USERTYPE_SESSION_KEY = "LIFERAY_SHARED_CPM_USERTYPE";
+	public static final String USER_PERMISSION_PARAM_NAME = "LIFERAY_SHARED_USER_PERMISSION";
+	//
+	// JAVA API
+	//
+
+	//
+	// state indicator methods
+	//
+
+	//
+	// action methods
+	//
+
+	//
+	// misc.
+	//
+
+	//
+	// abstract/interface methods
+	//
+
+	//
+	// accessor methods
+	//
+
+	//
+	// inner classes
+	//
+
+}
